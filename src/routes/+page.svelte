73b05489b4b8780d639d30e3ae9f7bd6a42e3700
CorbbinGoldsmith <CorbbinGoldsmith@users.noreply.github.com>
2023-04-08 18:11:33 +0200

    changed mobile appearance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,7 +65,7 @@ function bulkCreate() {
 				type="submit" 
 				on:click={handleGenerate}
 				>Generate</button>
-			<div class=" flex flex-col">
+			<div class=" lg:flex flex-col">
 				<div class="tracking-[.25em] text-gray-400 text-sm">
 					RESULT
 				</div>
@@ -129,7 +129,7 @@ function bulkCreate() {
 				>Generate Bulk ({amount})</button>
 		</div>
 	</form>
-	<div class="grid p-3 bg-gray-300 rounded mt-6 lg:grid-cols-4 py-4 font-mono gap-10">
+	<div class="grid p-3 bg-gray-300 rounded mt-6 lg:grid-cols-3 py-4 font-mono gap-10">
 
 		{#each generated_list as word} 
 			<div class="overflow-wrap break-words">{word}</div>
